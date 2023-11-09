12392c04cd207eb72c86db3aade55d3464ccc396
JosephWoodside <33580422+JosephWoodside@users.noreply.github.com>
2023-11-08 21:10:12 -0500

    Add files via upload

---
+++ dist/index.d.ts
@@ -0,0 +1,12 @@
+import '../node_modules/@c2pa/adobe-wc/dist/components/ManifestSummary/ManifestSummary';
+import "../node_modules/@c2pa/adobe-wc/dist/components/Tooltip/Tooltip";
+import '../node_modules/@c2pa/adobe-wc/dist/components/Indicator/Indicator';
+import '../node_modules/@c2pa/adobe-wc/dist/components/Popover/Popover';
+import '../node_modules/@c2pa/adobe-wc/dist/components/Icon/Icon';
+import '../node_modules/@c2pa/adobe-wc';
+export declare const getImgTags: () => {
+    el: HTMLImageElement;
+    url: string;
+}[];
+export declare const initCRLite: () => Promise<C2pa>;
+//# sourceMappingURL=index.d.ts.map
